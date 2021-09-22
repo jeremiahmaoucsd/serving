1f70c9dd28533a155fe17b19c3ad77574910dd69
Jeremiah Mao <jeremiahtmao@gmail.com>
2021-09-21 20:47:35 -0700

    moved block deleter and made todo for event executor

--- tensorflow_serving/util/net_http/server/public/server_request_interface.h
+++ tensorflow_serving/util/net_http/server/public/server_request_interface.h
@@ -39,21 +39,22 @@ namespace tensorflow {
 namespace serving {
 namespace net_http {
 
-// To be used with memory blocks returned via std::unique_ptr<char[]>
-struct BlockDeleter {
- public:
-  BlockDeleter() : size_(0) {}  // nullptr
-  explicit BlockDeleter(int64_t size) : size_(size) {}
-  inline void operator()(char* ptr) const {
-    // TODO: c++14 ::operator delete[](ptr, size_t)
-    std::allocator<char>().deallocate(ptr, static_cast<std::size_t>(size_));
-  }
-
- private:
-  int64_t size_;
-};
-
 class ServerRequestInterface {
+
+  // To be used with memory blocks returned via std::unique_ptr<char[]>
+  struct BlockDeleter {
+   public:
+    BlockDeleter() : size_(0) {}  // nullptr
+    explicit BlockDeleter(int64_t size) : size_(size) {}
+    inline void operator()(char* ptr) const {
+      // TODO: c++14 ::operator delete[](ptr, size_t)
+      std::allocator<char>().deallocate(ptr, static_cast<std::size_t>(size_));
+    }
+
+   private:
+    int64_t size_;
+  };
+
  public:
   virtual ~ServerRequestInterface() = default;
 
